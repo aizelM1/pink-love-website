cb002b80db1da61a50d994b1ebfe2bfbcaa58dde
aizelM1 <aizelmangune7@gmail.com>
2026-07-14 21:34:14 +0800

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -1,12 +1,10 @@
 export const firebaseConfig = {
-  apiKey: "",
-  authDomain: "",
-  projectId: "",
-  storageBucket: "",
-  messagingSenderId: "",
-  appId: ""
+  apiKey: "YOUR_API_KEY",
+  authDomain: "pink-love-website.firebaseapp.com",
+  projectId: "pink-love-website",
+  storageBucket: "pink-love-website.firebasestorage.app",
+  messagingSenderId: "82135610652",
+  appId: "1:82135610652:web:825ed1e33606c1f40576d9"
 };
 
-export function hasFirebaseConfig() {
-  return Object.values(firebaseConfig).every((value) => value && !value.includes("YOUR_"));
-}
+export const hasFirebaseConfig = true;
