a34efd9772a900be734f78350669cf8c3a5d421f
aizelM1 <aizelmangune7@gmail.com>
2026-07-14 21:44:10 +0800

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -1,10 +1,10 @@
-export const firebaseConfig = {
-  apiKey: "YOUR_API_KEY",
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyCaxhgr7ds3Z3vwwvfxRCmNPwYM8GiuAXs",
   authDomain: "pink-love-website.firebaseapp.com",
   projectId: "pink-love-website",
   storageBucket: "pink-love-website.firebasestorage.app",
   messagingSenderId: "82135610652",
-  appId: "1:82135610652:web:825ed1e33606c1f40576d9"
+  appId: "1:82135610652:web:825ed1e33606c1f40576d9",
+  measurementId: "G-8C7WWSMLZ7"
 };
-
-export const hasFirebaseConfig = true;
